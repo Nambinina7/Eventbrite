9f9381ac69655a011e15c8642049f5cdfbee1a54
Nambinina <nambinina.solofonaina@gmail.com>
2019-08-20 16:29:49 +0300

    Finalisation projet mardie 13 aout 2019

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   # get 'event/new'
   # get 'event/create'
   devise_for :users
+  # resources :users, only: :show
 	root "event#index"
  #  get 'events_controller/index'
  #  resources :events_controller
   resources :event
-  resources :users, only: :show
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
