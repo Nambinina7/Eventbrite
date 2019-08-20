a15ccb848eda79c2744d180f8d6d3c45aefc71ce
Nambinina <nambinina.solofonaina@gmail.com>
2019-08-20 15:53:39 +0300

    Finalisation projet mardie 13 aout 2019

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
  #  get 'events_controller/index'
  #  resources :events_controller
   resources :event
-  resources :users
+  resources :users, except: :destroy
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
